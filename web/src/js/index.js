a2404827d2c8a16c1b25eea3c7ead6a030589305
SamuelZambrana <samu-1994@hotmail.es>
2024-03-07 12:04:50 +0100

    Modificacion Proyecto

--- web/src/js/index.js
+++ web/src/js/index.js
@@ -17,91 +17,91 @@ if (typeof window.ethereum == 'undefined') {
 
     // Add your data HERE!
 
-    const BANK_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const BANK_ADDRESS = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512";
     const BANK_ABI = [
         {
             "inputs": [],
             "stateMutability": "nonpayable",
             "type": "constructor"
-        },
-        {
+          },
+          {
             "inputs": [],
             "name": "deposit",
             "outputs": [],
             "stateMutability": "payable",
             "type": "function"
-        },
-        {
+          },
+          {
             "inputs": [],
             "name": "getMyBalance",
             "outputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "",
-                    "type": "uint256"
-                }
+              {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+              }
             ],
             "stateMutability": "view",
             "type": "function"
-        },
-        {
+          },
+          {
             "inputs": [],
             "name": "getMyInterest",
             "outputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "",
-                    "type": "uint256"
-                }
+              {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+              }
             ],
             "stateMutability": "view",
             "type": "function"
-        },
-        {
+          },
+          {
             "inputs": [
-                {
-                    "internalType": "address",
-                    "name": "user",
-                    "type": "address"
-                }
+              {
+                "internalType": "address",
+                "name": "user",
+                "type": "address"
+              }
             ],
             "name": "getUserBalance",
             "outputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "",
-                    "type": "uint256"
-                }
+              {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+              }
             ],
             "stateMutability": "view",
             "type": "function"
-        },
-        {
+          },
+          {
             "inputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "newRate",
-                    "type": "uint256"
-                }
+              {
+                "internalType": "uint256",
+                "name": "newRate",
+                "type": "uint256"
+              }
             ],
             "name": "setInterestRate",
             "outputs": [],
             "stateMutability": "nonpayable",
             "type": "function"
-        },
-        {
+          },
+          {
             "inputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "amount",
-                    "type": "uint256"
-                }
+              {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+              }
             ],
             "name": "withdraw",
             "outputs": [],
             "stateMutability": "nonpayable",
             "type": "function"
-        }
+          }
     ];
 
     BANK = new web3.eth.Contract(BANK_ABI, BANK_ADDRESS);
